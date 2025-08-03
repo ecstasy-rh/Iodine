06b9eeeeea6a7de5206b05b654ced5683e9bca75
Exa <puntillol@linuxmail.org>
2025-08-02 23:48:35 -0400

    Switch to Chromium

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,14 +11,8 @@ dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release
 dnf5 -y install @gnome-desktop
 systemctl enable gdm
 
-# Install Chrome
-echo '[google-chrome]
-name=google-chrome
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub' | tee /etc/yum.repos.d/google-chrome.repo
-dnf5 -y install google-chrome-stable
+# Install Chromium
+dnf5 -y install chromium fedora-chromium-config
 
 # Keyd for cb keyboard mapping
 dnf5 -y copr enable alternateved/keyd
